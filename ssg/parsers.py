79bf19bd310f467e87d89a0c86fa23f02160c967
rivenzhang <ruiwen.zhang@sas.com>
2023-05-01 15:43:41 -0400

    correct1

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -47,7 +47,7 @@ class MarkdownParser(Parser):
         content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path, dest, html)
-        filtered = hooks.filter("generate_menu", html, self.base_ext)
+        filtered = hooks.filter("generate_menu", html["html_body"], self.base_ext)
         self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
